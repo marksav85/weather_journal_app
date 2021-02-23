cee868e0fe7c00f6a8ed4462e7c660a6dbd492e1
marksav85 <mark.saville@outlook.com>
2021-02-23 11:20:04 +0100

    Add base get function, post function and listener

--- website/app.js
+++ website/app.js
@@ -1,3 +1,67 @@
+// Personal API Key for OpenWeatherMap API
+let baseURL =
+let apiKey =
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+/* Function called by event listener */
+function performAction(e){
+const toDO =  document.getElementById('alsoToDO').value;
+getAnimal(baseURL,toDO, apiKey)
+}
+
+/* Function to GET Web API Data*/
+const toDO = async (baseURL, alsoToDO, key)=>{
+const res = await fetch(baseURL+alsoToDO+key)
+  try {
+const data = await res.json();
+    console.log(data)
+    return data;
+  }  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
+
+/* Function to POST data */
+const postData = async ( url = '', data = {})=>{
+  console.log(data)
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+    try {
+      const newData = await response.json();
+      // console.log(newData);
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
+}
+
+postData('/addAnimal', {animal:'horse'});
+
+/* Function to GET Project Data */
+const retrieveData = async (url='') =>{
+  const request = await fetch(url);
+  try {
+  // Transform into JSON
+  const allData = await request.json()
+  }
+  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+
 /* Global Variables */
 
 // Create a new date instance dynamically with JS
