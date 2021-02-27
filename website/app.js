90939702a0ea4e34009e5ea09a0ca9d94340a8b6
marksav85 <mark.saville@outlook.com>
2021-02-27 19:11:32 +0100

    Add var and function to get api content

--- website/app.js
+++ website/app.js
@@ -1,21 +1,28 @@
 /*
 
 // Personal API Key for OpenWeatherMap API
-let baseURL = api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+let baseURL = api.openweathermap.org/data/2.5/weather?zip={city name}&appid={API key}
 let apiKey = 261d1154112d39d61aa67532a79fcb31
+
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+/*let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performAction);
 
 /* Function called by event listener
 function performAction(e){
-const toDO =  document.getElementById('alsoToDO').value;
-getAnimal(baseURL,toDO, apiKey)
+const zipCode =  document.getElementById('zip').value;
+const mood = document.getElementById('feelings').value;
+getWeather(baseURL,zipCode, apiKey)
 }
 
 /* Function to GET Web API Data
-const toDO = async (baseURL, alsoToDO, key)=>{
-const res = await fetch(baseURL+alsoToDO+key)
+const getWeather = async (baseURL, zipCode, key)=>{
+const res = await fetch(baseURL+zipCode+''&appid='+key)
   try {
 const data = await res.json();
     console.log(data)
@@ -62,10 +69,3 @@ const retrieveData = async (url='') =>{
     // appropriately handle the error
   }
 };
-
-
-/* Global Variables */
-
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
